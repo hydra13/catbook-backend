2ff62440111d95c653d0014b6e490909540f0147
hydra13 <rob.sabirov@yandex.ru>
2020-07-01 23:14:05 +0300

    feat: add travis config, jest tests and refactoring

--- src/index.js
+++ src/index.js
@@ -1,37 +1,5 @@
-const path = require('path')
-const express = require('express');
-const favicon = require('serve-favicon');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const cats = require('./routes/cats');
-
-const app = express();
+const app = require('./app');
 const port = process.env.PORT || 7713;
-
-app.use(morgan('combined'));
-
-app.use(cors());
-
-app.use(favicon(path.resolve(__dirname, '..', 'public', 'favicon.ico')));
-
-app.use(bodyParser.json());
-
-app.use('/cats', cats);
-
-app.get('/', (req, res) => {
-    res.status(200)
-        .type('application/json')
-        .send('{ "error": false, "message": "CatBook backend server"}')
-        .end();
-})
-
-app.all('*', (req, res) => {
-    res.status(404)
-        .type('application/json')
-        .send('{ "error": true, "message": "Error 404. Not found"}')
-        .end();
-})
 
 app.listen(port, () => console.log(`
 CatBook backend server started: 
